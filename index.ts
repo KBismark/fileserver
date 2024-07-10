a75e439c5ee00eba643f3e4e32e80dba82798a21
KBismark <bismarkkwabenayamoah@gmail.com>
2024-07-10 23:45:00 +0000

    Add error handler

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
-import express from 'express';
+import express, {Response} from 'express';
 import cors from 'cors'
+import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import morgan from 'morgan'
 import {join} from 'path'
 import {createWriteStream} from 'fs'
-import { connectToDatabase } from './db-connection';
 import { authRouter } from './routes/auth';
 import { authenticateRequest } from './middleware';
 import { PageData } from './routes/data/Page';
 import { adminRouter } from './routes/admin';
 import { ADMIN_ACCOUNT, IS_DEVELOPMENT, PORT } from './utils/constants';
+import { databaseConnection, publicContentDir } from './db-connection';
+import { ReesponseCodes } from './utils/response_codes';
 
 
-const publicContentDir = join(__dirname, IS_DEVELOPMENT? '/client/build' : '../client/build')
-
-// App and server exported for testing purpose
+// App exported 
 export const app = express();
 // Handle CORS
 app.use(cors());
@@ -23,12 +23,14 @@ app.use(cors());
 app.use(cookieParser());
 // Compress large files for easy download
 app.use(compression())
-// Pares JSON resposes
+// Parse JSON resposes
 app.use(express.json())
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Logs application access to a log.txt file
 const writableStream = createWriteStream(join(__dirname, '/logs.txt'), { flags: 'a' });
-app.use(['/auth/:route', '/content', '/resource/:route'], morgan('combined', { stream: writableStream }));
+app.use(['/auth/:route', '/content', '/resource/:route', '/admin/:route'], morgan('combined', { stream: writableStream }));
 
 
 const serveBaseUrl = (res: any)=>{
@@ -70,7 +72,7 @@ app.get('/admin', authenticateRequest, (req, res)=>{
         // User is authenticated. Serve page
        return serveBaseUrl(res)
     }
-    
+    res.redirect('/');
 })
 
 app.get('/resource/data', authenticateRequest, PageData)
@@ -84,20 +86,18 @@ app.use('/auth', authRouter);
 // Mount adim route
 app.use('/admin', adminRouter);
 
-startApp(); // Starts the server
-function startApp(){
-    connectToDatabase().then((response)=>{
-        app.listen(PORT, ()=>{
-            console.log(`App successfully started!`);
-            
-        })
-    }).catch((error)=>{
-        // Can't connect to database
-        console.log(error);
-        
+// Handle errors passed by express
+app.use((err:any, req:any, res: Response, next:any)=>{
+    res.status(ReesponseCodes.notFound).end()
+})
+
+// Start server after database connection
+databaseConnection.then((connection)=>{
+    app.listen(PORT, ()=>{
+        console.log(`App successfully started!`);
     })
-}
-
-
-
-
+})
+.catch((err)=>{
+    // Can't connect to database
+    throw err;
+})
