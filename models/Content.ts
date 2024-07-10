21e192e9b581bf21961b0e12d896e86525c58291
KBismark <bismarkkwabenayamoah@gmail.com>
2024-07-10 23:47:07 +0000

    Add file schema

--- models/Content.ts
+++ models/Content.ts
@@ -5,12 +5,22 @@ type Upload = {
     _id: string;
     downloadCount: number; 
     shareCount: number;
+    description: string;
+    title: string;
+    type: string;
+    suffix: string;
+    time: Number;
 }
 
 const UploadSchema = new mongoose.Schema<Upload>({
   _id: { type: String, required: true }, // File_id
   downloadCount: { type: Number, default: 0 },
-  shareCount: { type: Number, default: 0 }
+  shareCount: { type: Number, default: 0 },
+  description: { type: String, required: true },
+  title: { type: String, required: true },
+  suffix: { type: String, required: true }, // File extension
+  time: {type: Number},
+  type: {type: String, required: true}
 });
 
 export const Uploader = mongoose.model<Upload>('filedata', UploadSchema);
